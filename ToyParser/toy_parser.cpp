270a2bcb01d8efef12a883d2d779647dfc229432
djw96 <jellybeanmx@gmail.com>
2016-11-17 09:13:17 +0800

    add part of parse_identifier();

--- ToyParser/toy_parser.cpp
+++ ToyParser/toy_parser.cpp
@@ -1,8 +1,22 @@
 #include"toy_parser.h"
-#include<iostream>
 using namespace std;
 
-int main() {
+toy_type parse_number(const char *str,toy_value v) {
 
-	cout << "hello world!"<<endl;
+}
+
+int parse_identifier(const char *str, toy_value v) {
+	const char *p = str;
+	if (*p == 'e')
+	{
+		if (p[1] != 'l' || p[2] != 's' || p[3] != 'e')
+			return PARSE_INVALID_VALUE;
+	}
+	else 
+	{
+		v.str = str;
+		v.type = TOY_IDENTIFIER;
+	}
+		
+	return PARSE_OK;
 }
--- ToyParser/toy_parser.h
+++ ToyParser/toy_parser.h
@@ -1,10 +1,15 @@
 #ifndef TOY_PARSER_H_
-#define TOY_PARSER_H_
+#define TOY_PARSER_H
+#include<string>
 enum toy_type{TOY_COMMENTS,TOY_KEYWORD,TOY_IDENTIFIER,
 						TOY_OPERATOR,TOY_DELIMITER, TOY_NUMBER};
 
+enum {
+	PARSE_INVALID_VALUE,
+	PARSE_OK
+};
 struct toy_value {
+	string str;
 	toy_type type;
-
 };
 #endif
