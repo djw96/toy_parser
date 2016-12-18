cbd42d3d5b7bd37ee2c9a3e978e1f44874a490bd
djw96 <jellybeanmx@gmail.com>
2016-12-18 15:35:02 +0800

    remove test project and modify parser project

--- ToyParser/toy_parser.cpp
+++ ToyParser/toy_parser.cpp
@@ -1,62 +1,43 @@
 #include"toy_parser.h"
 #include<iostream>
-using namespace std;
+//using namespace std;
+
 #define ISDIGIT(n) (n>='0'&&n<='9')
 #define ISLETTER(n) ((n>='A'&&n<='Z')||(n>='a'&&n<='z'))
-void  parse_identifier(const char *str,toy_value *v) {
-	if (ISLETTER(str[0])) {
-		for (int i = 1; str[i]; i++)
-		{
-			if (!ISDIGIT(str[i]) && !ISLETTER(str[i]))
-				return;
-		}
-		v->str = str;
-		v->type = TOY_IDENTIFIER;
-	}
-	else
-		return;
-}
 
-int parse_keyword(const char *str, const char *k, toy_value *v) {
-	
-	if (str == k)
-		v->type = TOY_IDENTIFIER;
-	else{
-		if (v->str.size() > 0&&v->str.size()<=64)
-			return PARSE_OK;
-		else
-			return PARSE_INVALID_VALUE;
+void add_token(int len,toy_type t)
+{
+
+}
+int parse_line(const char *begin) 
+{
+	auto line=make_shared<toy_line>();
+	switch (*begin)
+	{
+	case'+':
+		add_token(1, TOY_OPERATOR);
+		break;
+	case'-':
+			add_token(1, TOY_OPERATOR);
+			break;
+	case'/':
+		add_token(1, TOY_OPERATOR);
+		break;
+	case'*':
+		add_token(1, TOY_OPERATOR);
+		break;
+	case'=':
+		add_token(1, TOY_OPERATOR);
+		break;
+	default:
+		return PARSE_INVALID_VALUE;
+
 	}
-	v->str = str;
 	return PARSE_OK;
 }
 
-int parse_value(const char *str, toy_value *v)
+int main()
 {
-	switch (str[0])
-	{
-	case'e':
-		return parse_keyword(str, "else", v);
-	case'i':
-		if (str[1] == 'n')
-			return parse_keyword(str, "int", v);
-		else if (str[1] == 'f')
-			return parse_keyword(str, "if", v);
-		else
-			return PARSE_INVALID_VALUE;
-	case'r':
-		return parse_keyword(str, "real", v);
-	case't':
-		return parse_keyword(str, "then", v);	
-	case'w':
-		return parse_keyword(str, "while", v);
-	default:
-		return PARSE_OK;
-	}
-}
-
-int parse(const char *str, toy_value *v)
-{
-	parse_identifier(str,v);
-	return parse_value(str, v);
+	cout << "123";
+	return 0;
 }
--- ToyParser/toy_parser.h
+++ ToyParser/toy_parser.h
@@ -1,19 +1,34 @@
-#ifndef TOY_PARSER_H_
+#ifndef TOY_PARSER_H
 #define TOY_PARSER_H
+
+#include<memory>
+#include<vector>
 #include<string>
-enum toy_type{TOY_COMMENTS,TOY_KEYWORD,TOY_IDENTIFIER,
+using namespace std;
+enum toy_type{TOY_UNKNOWN,TOY_COMMENTS,TOY_KEYWORD,TOY_IDENTIFIER,
 						TOY_OPERATOR,TOY_DELIMITER, TOY_NUMBER};
 
 enum {
 	PARSE_OK,
 	PARSE_INVALID_VALUE
 };
-struct toy_value {
-	std::string str;
-	toy_type type=TOY_COMMENTS;
+
+struct toy_token {
+	typedef  vector<toy_token>  List;
+	string str;
 	int row = -1;
 	int column = -1;
+	toy_type type = TOY_UNKNOWN;
 };
-int parse(const char *str, toy_value *v);
+
+struct toy_line {
+	typedef shared_ptr<toy_line> Ptr;
+	typedef vector<Ptr> List;
+	toy_token::List tokens;
+};
+struct toy_file {
+
+};
+//int parse(const char *str, toy_token *v);
 
 #endif
